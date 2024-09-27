18f2d301e5f4a94dbcfde61a2592bb7e57c6006b
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-09-27 18:04:32 +0530

    Add files via upload
    My first ever python file!! 27/09/2024!

---
+++ new.py
@@ -0,0 +1,26 @@
+x = 100
+y = 200
+
+if x < y:
+	
+ print("hemanthkumar")
+
+
+
+
+
+
+# a = 33
+# b = 200
+# if b > a:
+#   print("b is greater than a")
+
+
+
+# x = 100
+# y = 300
+
+# if x < y:
+
+# print("this is hemanth kumar")
+  

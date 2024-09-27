2d8ea892c1bc63ef7c7060c5b17b7b2e4f7f6343
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-09-27 21:44:54 +0530

    Add files via upload
    # i like python programming tooooooooooo......

--- new.py
+++ new.py
@@ -1,26 +1,41 @@
-x = 100
-y = 200
-
-if x < y:
-	
- print("hemanthkumar")
-
-
-
-
-
-
+# x = 100
+# y = 200
+# if x < y:
+#  print("hemanthkumar")
 # a = 33
 # b = 200
 # if b > a:
 #   print("b is greater than a")
-
-
-
 # x = 100
 # y = 300
-
 # if x < y:
-
 # print("this is hemanth kumar")
   
+
+
+# a = 400
+# b = 500
+
+# if a>b:
+#   print("nagaland")
+# elif a==b:
+#   print("this is hemanth kumar")
+# else:
+#  print("nalavada lo programming language")
+
+
+
+#short hanf if else
+
+# a = 100
+# b = 600
+
+# # if a<b : print("eshanarang and samarasaigal")
+ 
+# print("a") if a>b else print("b")
+
+# i will leave this here today. so i like python too so i'mn just gonna leave it here and i want to earn java  script now and start my day to it.
+
+# for sure python programming is also sooo good...
+
+
